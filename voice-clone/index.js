01b6aaeea3b5e285bb2ef624df35849f8d017357
Kavish Nayeem <nayeemda0411@gmail.com>
2025-08-30 02:41:07 -0500

    Minor cors fix

--- voice-clone/index.js
+++ voice-clone/index.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Accept-Charset'],
+}));
 
 const {
   XI_API_KEY,
